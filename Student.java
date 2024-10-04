9c7a7d70d1e87107e91e7f574bf39380cd9c3916
Archanamd <149602874+archanamd12@users.noreply.github.com>
2024-10-04 20:34:48 +0530

    Add files via upload

--- Student.java
+++ Student.java
@@ -1,4 +1,4 @@
-
+package app;
 import java.util.Scanner;
 public class Student {
     int id;
@@ -7,38 +7,34 @@ public class Student {
     float p;
     float m;
     float avg;
-    Student(int id,String name,float c,float p, float m){
+    Student(int id,String name,float c,float p, float m,float avg){
 
         this.id=id;
         this.name=name;
         this.c=c;
         this.p=p;
         this.m=m;
-       
+        this.avg=avg;
     }
     public void printdata(){
         System.out.println("id "+id);
-        System.out.println("Name: " + name);
-        System.out.println("Average Marks: " + findAvg());
     }
-    public float findAvg(){
+    public float findavg(){
         return  (c+m+p)/3;
     }
-    
+    class Tester{
         public static void main(String[] args) {
-
+            Student e1=new Student;
             Scanner sc= new Scanner(System.in);
-            System.out.println("enter id:");
+            System.out.println("enter id");
            
             int id=sc.nextInt();
             String name=sc.next();
-            float c=sc.nextFloat();
-            float m=sc.nextFloat();
-            float p=sc.nextFloat();
-            Student e1=new Student(id,name,c,p,m);
-            System.out.println("avg marks is: "+ e1.findAvg());
-            e1.printdata();
+            int c=sc.nextInt();
+            int m=sc.nextInt();
+            int p=sc.nextInt();
+            System.out.println("avg marks is "+ findavg());
         }
     }
     
-
+}
