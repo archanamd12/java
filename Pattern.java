137f4676427c1b061a7a3e13c4f14186d7dedbdc
Archanamd <149602874+archanamd12@users.noreply.github.com>
2024-12-28 13:03:23 +0000

    Add <filename>

---
+++ Pattern.java
@@ -0,0 +1,19 @@
+public class Pattern
+{
+	public static void main(String[] args) {
+	    int i=0,j=0;
+	    for(i=0;i<4;i++){
+	         for(j=0;j<5;j++){
+	             if(i== 0||i==3||j==0||j==4){
+	                 
+	        	System.out.print("*");
+	        	
+	             } else{
+	        System.out.print(" ");
+	    }
+	     
+	         }
+	System.out.println( );
+}
+}
+}
