470b2c3a3d0a918161cbfb986edcf15b38200971
Archanamd <149602874+archanamd12@users.noreply.github.com>
2025-01-02 06:47:58 +0000

    Your commit message here

---
+++ Inverted_half_pyramid.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Inverted_half_pyramid{
+    public static void main(String args[]){
+int i=0,j=0;
+Scanner sc=new Scanner(System.in);
+int m=sc.nextInt();
+for(i=4;i>0;i--){
+for(j=0;j<i;j++){
+    System.out.print("*");
+
+}
+System.out.println();
+}
+
+
+
+
+    }
+}
